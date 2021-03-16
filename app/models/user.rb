fa9ab392a13e83dce0ab4b927d50908d1faced12
sukepio <naoto927sukena@gmail.com>
2021-03-16 12:48:40 +0000

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,21 +80,27 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
-  #ユーザーをフォローする 
+  #ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id =:user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end
+
+  #ユーザーをフォローする
   def follow(other_user)
     following << other_user
   end
-  
+
   #ユーザーをフォロー解除する
   def unfollow(other_user)
-    active_relationships.find_by(followed_id: other_user.id).destroy  
+    active_relationships.find_by(followed_id: other_user.id).destroy
   end
-  
+
   #現在のユーザーがフォローしていたらtrueを返す
   def following?(other_user)
     following.include?(other_user)
   end
-  
+
   private
 
   # メールアドレスをすべて小文字にする
